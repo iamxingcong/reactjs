cce9fc456b7cc8067ae095407e8c7661d1bb8545
ngongu <121950263@qq.com>
2020-01-11 02:31:17 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import React, { Suspense } from 'react';
+import './App.css';
 
 const Home = React.lazy(() => import('./routes/Home'));
 
 const About = React.lazy(() => import('./routes/About'));
 
 
-
 function App() {
   return (
    <Router>
     <Suspense fallback={<div>Loading...</div>}>
+    	<div id='menu'>
 		<Link to="/">Home</Link>
 		<Link to="/about"> about </Link>
+		</div>
 		<Switch>
 			<Route exact path="/" component={Home}/>
 			<Route path="/about" component={About}/>
