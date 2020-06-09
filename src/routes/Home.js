9e51c8efc14160b33eea14bdaabdf6a55c67650b
unknown <121950263@qq.com>
2020-06-09 12:24:44 +0700

    update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
 import React  from 'react';
-import { Table } from 'antd';
+import { Table, Button, Modal, Form, Input, Checkbox  } from 'antd';
 const columns = [
   {
     title: '音乐名称',
@@ -18,6 +18,20 @@ const columns = [
   }
 ];
 var data = []
+
+
+
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
+
+
 class Home extends React.Component {
  
   constructor(props) {
@@ -28,6 +42,38 @@ class Home extends React.Component {
       items: []
     };
   }
+
+  state = { visible: false };
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+
+
+  onFinish = values =>  {
+    console.log('Success:', values);
+  };
+
+ onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+
+
 
   componentDidMount() {
   
@@ -66,7 +112,21 @@ class Home extends React.Component {
       return
     }
   }
- 
+  handleClick (v) {
+    
+    fetch('/add', {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'id=' + v
+    }).then(function(response) {
+      console.log(response);
+    });
+
+  }
   render() {
     const { error, isLoaded, items } = this.state;
     if (error) {
@@ -76,19 +136,65 @@ class Home extends React.Component {
     } else {
       return (
         <div>
-        <Table columns={columns} dataSource={ data } size="small" /> 
-         <ul>
-          {items.map(item => (
-            <li key={ item.id }>
-              {item.name}
-            </li>
-          ))}
-        </ul> 
+           <Button type="primary"  onClick={this.showModal}> 添加 </Button>
+          <Table columns={columns} dataSource={ data } size="small" /> 
+          <ul>
+            {items.map(item => (
+              <li key={ item.id }>
+                {item.name}
+                <button  onClick={() => this.handleClick(item.id)}> 删除 </button>
+              </li>
+              
+            ))}
+          </ul>
+
+          <Modal
+            title="添加数据"
+            visible={this.state.visible}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+          >
+            
+            <Form
+                {...layout}
+                name="basic"
+                initialValues={{ remember: true }}
+                onFinish={this.onFinish}
+                onFinishFailed={this.onFinishFailed}
+              >
+                <Form.Item
+                  label="Username"
+                  name="username"
+                  rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <Form.Item
+                  label="Password"
+                  name="password"
+                  rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                  <Input.Password />
+                </Form.Item>
+
+                <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <Form.Item {...tailLayout}>
+                  <Button type="primary" htmlType="submit">
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
+              
+          </Modal>
+
         </div>
       );
     }
   }
 }
 
- 
 export default  Home;
